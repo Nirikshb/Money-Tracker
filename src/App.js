3412098b81f64f779008d6b5511be921563f9b19
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-06-06 22:05:15 +0530

    solution

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// Take Home Assignment
 
-function App() {
+// 1. Write reducer for UPDATE_EXPENSE action.
+// 2. Make Month and Year filters functional.
+// 3. Add options in Navbar to move through the application
+// 4. Handle wrong credentials login error message
+// 5. Create Reset Filters button
+
+import React from "react";
+
+import Login from "./components/Login/Login";
+
+export const centeredStyle = {
+  width: "100%",
+  height: "100vh",
+  display: "grid",
+  placeItems: "center",
+  background: "dodgerblue",
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Home Page
+      <Login />
     </div>
   );
-}
+};
 
 export default App;
