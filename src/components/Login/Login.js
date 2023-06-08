21b4130a8d8fcf869bb36f4c4a1acaa4a67e1d75
Niriksh Bharti <111577030+Nirikshb@users.noreply.github.com>
2023-06-08 17:59:30 +0530

    solution

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,68 +1,9 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-
-import {
-  Card,
-  CardHeader,
-  CardBody,
-  Text,
-  Heading,
-  Input,
-  Button,
-} from "@chakra-ui/react";
-
-
-import { centeredStyle } from "../../App";
+import React from 'react'
 
 const Login = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
-  const navigate = useNavigate();
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    name === "email" ? setEmail(value) : setPassword(value);
-  };
-
-
   return (
-    <div style={centeredStyle}>
-      <Card variant="elevated" size="lg" maxW="md" width="100%">
-        <CardHeader>
-          <Heading size="md">Login</Heading>
-        </CardHeader>
+    <div>Login</div>
+  )
+}
 
-        <CardBody>
-          <>
-            <Text mb="8px">Email</Text>
-            <Input
-              value={email}
-              name="email"
-              onChange={handleChange}
-              size="md"
-            />
-          </>
-          <>
-            <Text mt={4} mb="8px">
-              Password
-            </Text>
-            <Input
-              // type="password"
-              value={password}
-              name="password"
-              onChange={handleChange}
-              size="md"
-            />
-          </>
-          <Button mt={4} ml={40} colorScheme="twitter" onClick={handleLogin}>
-            Log In
-          </Button>
-        </CardBody>
-      </Card>
-    </div>
-  );
-};
-
-export default Login;
+export default Login
